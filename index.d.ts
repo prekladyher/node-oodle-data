8e28ff5b4a58ce6a192d70a97851b633060c05b3
Pavel Horal <pavel.horal@orchitech.cz>
2023-03-12 20:26:05 +0100

    feat: Allow choosing compressor type.

--- index.d.ts
+++ index.d.ts
@@ -5,12 +5,13 @@ declare const Noodle: {
 
     /**
      * Compress raw input byte array and write compressed data to the output byte array.
+     * @param type Compressor reference.
      * @param input Input raw data array.
      * @param length Input data byte length.
      * @param output Output compressed byte array.
      * @return Compressed data length or zero when compression failed.
      */
-    Compress: (input: Uint8Array, length: number, output: Uint8Array) => number;
+    Compress: (type: number, input: Uint8Array, length: number, output: Uint8Array) => number;
 
     /**
      * Decompress input byte array and write raw data to the output byte array.
@@ -20,6 +21,13 @@ declare const Noodle: {
      * @return Raw output data length or zero if unable to decompress 
      */
     Decompress: (input: Uint8Array, output: Uint8Array, length: number) => number;
+
+    /**
+     * Get compressor reference for the given chunk.
+     * @param input Input compressed data array.
+     * @returns Compressor reference (i.e. OodleLZ_Compressor enum value).
+     */
+    GetFirstChunkCompressor: (input: Uint8Array) => number;
 
 };
 
--- src/noodle.cc
+++ src/noodle.cc
@@ -4,18 +4,19 @@
 
 using namespace Napi;
 
-int OodleCompress(Napi::Uint8Array input, uint32_t length, Napi::Uint8Array output) {
-    return OodleLZ_Compress(OodleLZ_Compressor_Kraken, input.Data(), length, output.Data(), OodleLZ_CompressionLevel_Normal);
+int OodleCompress(uint32_t type, Napi::Uint8Array input, uint32_t length, Napi::Uint8Array output) {
+    return OodleLZ_Compress((OodleLZ_Compressor) type, input.Data(), length, output.Data(), OodleLZ_CompressionLevel_Normal);
 }
 
 Napi::Number Node_Compress(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
 
-    Napi::Uint8Array input = info[0].As<Napi::Uint8Array>();
-    uint32_t length = info[1].As<Napi::Number>().Uint32Value();
-    Napi::Uint8Array output = info[2].As<Napi::Uint8Array>();
+    uint32_t type = info[0].As<Napi::Number>().Uint32Value();
+    Napi::Uint8Array input = info[1].As<Napi::Uint8Array>();
+    uint32_t length = info[2].As<Napi::Number>().Uint32Value();
+    Napi::Uint8Array output = info[3].As<Napi::Uint8Array>();
 
-    int result = OodleCompress(input, length, output);
+    int result = OodleCompress(type, input, length, output);
 
     return Napi::Number::New(env, result);
 }
@@ -36,11 +37,22 @@ Napi::Number Node_Decompress(const Napi::CallbackInfo& info) {
     return Napi::Number::New(env, result);
 }
 
+Napi::Number Node_GetFirstChunkCompressor(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+
+    Napi::Uint8Array input = info[0].As<Napi::Uint8Array>();
+
+    int result = OodleLZ_GetFirstChunkCompressor(input.Data(), input.ByteLength(), NULL);
+
+    return Napi::Number::New(env, result);
+}
+
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
     int32_t OodleDecoderMemorySize = OodleLZDecoder_MemorySizeNeeded(OodleLZ_Compressor_Invalid, -1);
 
     exports.Set(Napi::String::New(env, "Compress"), Napi::Function::New(env, Node_Compress));
     exports.Set(Napi::String::New(env, "Decompress"), Napi::Function::New(env, Node_Decompress));
+    exports.Set(Napi::String::New(env, "GetFirstChunkCompressor"), Napi::Function::New(env, Node_GetFirstChunkCompressor));
     return exports;
 }
 
